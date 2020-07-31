da39e6eda6f8874b1a97ff00b1a25592db5837a3
Antonio Piccolboni <antonio@piccolboni.info>
2020-07-31 16:32:32 -0700

    catch up with some renaming, fix __all__

--- rew/__init__.py
+++ rew/__init__.py
@@ -1,26 +1,30 @@
 """Interface for this package."""
 from .rew import (  # noqa
+    Retry,
+    NamedRetry,
     checkpoint,
-    rewind,
-    checkpoint_each_line,
+    retry,
     checkpoint_each_function,
-    checkpoint_deco,
-    Checkpoint,
+    checkpoint_each_line,
     named_checkpoint,
     named_rewind,
+    rewind,
+    rewind_all,
 )
 
-all = [
+__all__ = [
+    "Retry",
+    "NamedRetry",
     "checkpoint",
-    "rewind",
-    "checkpoint_each_line",
+    "retry",
     "checkpoint_each_function",
-    "checkpoint_deco",
-    "Checkpoint",
+    "checkpoint_each_line",
     "named_checkpoint",
     "named_rewind",
+    "rewind",
+    "rewind_all",
 ]
 
 __author__ = """Antonio Piccolboni"""
-__email__ = "autosig@piccolboni.info"
+__email__ = "rew@piccolboni.info"
 __version__ = "__version__ = '0.1.0'"
